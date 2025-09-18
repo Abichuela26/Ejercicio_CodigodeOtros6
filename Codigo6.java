7282d7fe0d4b484e4987abebdb49e13796ea99a9
Abigail Ramírez Ortiz <abigailramirezortiz91@gmail.com>
2025-09-17 23:09:46 -0600

    Refactorización y correción de Codigo6.java

--- Codigo6.java
+++ Codigo6.java
@@ -1,24 +1,48 @@
-public class codigo6 {
-	  
-	    int[] n = int[20];
+/*
+ * Programa para generar un arreglo de 20 números enteros aleatorios
+ * entre 20 y 400.
+ * El usuario elige si desea resaltar los múltiplos de 5 o los múltiplos de 7. 
+ * 
+ * Posteriormente el programa vuelve a mostrar el arreglo, destacando los números seleccionados
+ * con corchete []. 
+ */
 
-	    for (int i = 0; i < 20; i+) {
-	      n[i] = (int)(Math.random() * 381) + 20;
-	      System.print(n[i] + " ");
-	    }
+//Imports utilizados
+import java.util.Scanner;
+
+public class Codigo6 {
+	  //Se agrega class main
+	public static void main (String[] args) {
+		//Se corrige la declaración del arreglo -> "int[] n= int[20];"
+	    int[] n = new int[20]; //Arreglo de 20 números enteros
 	    
-	    System.out.printl("\n¿Qué números quiere resaltar? ");
+	    //Se corrige el for, error de sintaxis -> "i+" en lugar de "i++"
+	    for (int i = 0; i < 20; i++) {
+	      n[i] = (int)(Math.random() * 381) + 20; //Números entre 20 y 400
+	      System.out.print(n[i] + " ");
+	    }
+	     
+	    //Faltaba salto de línea y la impresión estaba mal escrita
+	    System.out.println("\n¿Qué números quiere resaltar? ");
 	    System.out.print("(1 – los múltiplos de 5, 2 – los múltiplos de 7): ");
-	    int opcion = Integer.parseInt(System.console().readLine();
-
-	    int multiplo = (opcion == 1) : 5 ? 7;
-
-	    foreach (char e : n) {
+	    
+	    Scanner s = new Scanner(System.in);
+	    int opcion = s.nextInt();
+	    
+	    //Determinar el múltiplo según la opción, se corrige el ? y : , error de colocación
+	    int multiplo = (opcion == 1) ? 5 : 7;
+	    
+	    //Mostrar números resaltando los múltiplos seleccionados
+	    for (int e : n) {
 	      if (e % multiplo == 0) {
 	        System.out.print("[" + e + "] ");
-	       else {
-	        System.in.print(e + " ");
+	      } else {
+	    	  //Corrección de "in" por "out"
+	        System.out.print(e + " ");
 	      }
 	    }
-	  
-	}
+	    
+	    //Se agrega s.close para cerrar el Scanner
+	  s.close();
+	}//class main
+}//class Cogido 6
